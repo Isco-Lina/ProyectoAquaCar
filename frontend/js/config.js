471ab39b18442734cf27309628ae6d0b52659a57
Francisco Molina <molina.carrillo1996@gmail.com>
2026-07-06 21:12:25 -0400

    correccion URL backend

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,6 @@
 const LOCAL_API_BASE_URL = "http://localhost:3000/api";
-const PRODUCCION_API_BASE_URL = "https://aquacar-backen.onrender.com/api";
+const PRODUCCION_API_BASE_URL = "https://aquacar-api.onrender.com/api";
+
 const ES_LOCAL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1";
